6246635f195f035dee519338171663bc2f3c2e64
jithu843 <92719456+jithu843@users.noreply.github.com>
2025-04-23 20:03:26 +0530

    crud.py

---
+++ crud.py
@@ -0,0 +1,12 @@
+from . import models, schemas
+from sqlalchemy.orm import Session
+
+def create_employee(db: Session, emp: schemas.EmployeeCreate):
+    db_emp = models.Employee(**emp.dict())
+    db.add(db_emp)
+    db.commit()
+    db.refresh(db_emp)
+    return db_emp
+
+def get_employees(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Employee).offset(skip).limit(limit).all()
